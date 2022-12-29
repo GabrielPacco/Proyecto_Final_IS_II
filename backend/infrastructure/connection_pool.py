cce41477dd878a2bf2b44bdb228767dcbeda36c7
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-12-29 00:00:12 -0500

    new database

--- backend/infrastructure/connection_pool.py
+++ backend/infrastructure/connection_pool.py
@@ -3,11 +3,11 @@ import time
 import mysql.connector.pooling
 
 dbconfig = {
-    "host":"us-cdbr-east-06.cleardb.net",
+    "host":"127.0.0.1:5000",
     "port":"3306",
-    "user":"ba4af9cb7efdee",
-    "password":"010d7bb1",
-    "database":"heroku_7ee060115e9b2a7"
+    "user":"mysql-is",
+    "password":"1234",
+    "database":"tif"
 }
 
 class MySQLPool(object):

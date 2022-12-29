74a351867270fc893be0178dd9499addc99155eb
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2022-12-28 19:33:09 -0500

    test usuario

--- test/unitarias/test_evento.py
+++ test/unitarias/test_evento.py
@@ -1,19 +1,19 @@
 import unittest
-from ...backend.models import EventoModel
+from ...backend.models.evento import EventoModel
 
 class TestEvento(unittest.TestCase):
     def setUp(self):
         self.evento = EventoModel(2,2,2,"Ejemplo","Este es un ejemplo","link")
 
-    def test_set_nombre(self): #4
+    def test_set_nombre(self):
         self.evento.set_nombre("Nuevo nombre")
         self.assertEqual(self.evento.get_nombre(), "Nuevo nombre")
 
-    def test_set_detalles(self): #1
+    def test_set_detalles(self):
         self.evento.set_detalles("Nuevos detalles")
         self.assertEqual(self.evento.get_detalles(), "Nuevos detalles")
     
-    def test_set_link(self): #1
+    def test_set_link(self):
         self.evento.set_link("Nuevo link")
         self.assertEqual(self.evento.get_link(), "Nuevo link")
 
